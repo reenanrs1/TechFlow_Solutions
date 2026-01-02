5e7ec493a2377a1ad7a30bba33cc3d75f227894c
reenanrs1 <renan.victor325@outlook.com>
2026-01-02 11:12:17 -0300

    Criação do post  + rota /add, para criar uma nova tarefa.

--- src/app.py
+++ src/app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
 tasks = [] 
+
 @app.route('/')
-def index(): return "TechFlow Ativa"
+
+def index(): 
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    content = request.form.get('content')
+    if content:
+        tasks.append({'id': len(tasks) + 1, 'content': content})
+    return redirect(url_for('index'))
+
 if __name__ == '__main__': app.run(debug=True)
